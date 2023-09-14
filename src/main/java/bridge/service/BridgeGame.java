0b3df829d5b6097e2f73f863609eb08f98d33fcb
KHJHK <tom1997tom@gmail.com>
2023-09-15 03:41:19 +0900

    Feat: BridgeGame 클래스 작성
    BridgeGame 클래스 작성 완료

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,23 +1,46 @@
 package bridge.service;
 
+import bridge.domain.Bridge;
+import bridge.domain.Player;
+
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private Bridge bridge;
+    private Player player;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    public BridgeGame(Bridge bridge) {
+        this.bridge = bridge;
+        player = new Player();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    public int move(String move) {
+        player.addMove(move);
+        if(bridge.isSameUserInputAndBridge(move, player.getRound())){
+            if(isGameEnd()){
+                return 1;   //추후 WIN으로 변경
+            }else if(!isGameEnd()){
+                return 0;   //추후 PLAYING으로 변경
+            }
+        }
+        return 2; //추후 LOSE로 변경
+
+
+    }
+
     public void retry() {
+        player.initMove();
+        player.countTrys();
+    }
+
+    public boolean isGameEnd(){
+        return player.getMove().size() == bridge.getBridge().size();
+    };
+
+    public Player getPlayer() {
+        return player;
     }
 }
